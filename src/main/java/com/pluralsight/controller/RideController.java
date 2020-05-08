91195418559a89491b31337cd04eff33da5807fa
ChuckBTaylor <WLScripps@gmail.com>
2020-05-08 11:07:13 +0300

    Updating for new dependencies

--- src/main/java/com/pluralsight/controller/RideController.java
+++ src/main/java/com/pluralsight/controller/RideController.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pluralsight.model.Ride;
@@ -17,7 +16,7 @@ public class RideController {
 	@Autowired
 	private RideService rideService;
 	
-	@RequestMapping(value = "/rides", method = RequestMethod.GET)
+	@GetMapping(value = "/rides")
 	public @ResponseBody List<Ride> getRides() {
 		return rideService.getRides();
 	}
